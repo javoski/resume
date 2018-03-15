35f943cfaf802f4da863fc307cc29f35120add8e
javoski <xujiongbo@gmail.com>
2018-03-15 19:01:15 +0800

    use ora to show building status

--- scripts/build.js
+++ scripts/build.js
@@ -2,6 +2,7 @@ const fs = require('fs-extra')
 const path = require('path')
 const yaml = require('js-yaml')
 const Handlebars = require('handlebars')
+const ora = require('ora')
 
 const dataFile = './data.yml'
 const tplFile = './template.html'
@@ -9,20 +10,36 @@ const assetsPath = 'assets'
 const distPath = './dist'
 const distFileName = 'index.html'
 
+const spinner = ora('Reading data').start()
+
+function setState (msg, state) {
+  spinner.text = msg
+  switch (state) {
+    case 'success':
+      spinner.succeed()
+      break
+    case 'error':
+      spinner.fail()
+      break
+  }
+}
+
 async function build () {
   try {
     const [data, tpl] = await Promise.all([
       fs.readFile(dataFile, 'utf8'),
       fs.readFile(tplFile, 'utf8')
     ])
+    setState('Building resume')
     const renderFn = Handlebars.compile(tpl)
     const resumeData = yaml.safeLoad(data)
     await fs.ensureDir(distPath)
     const result = renderFn(resumeData)
     await fs.writeFile(path.resolve(distPath, distFileName), result, 'utf8')
     await fs.copy(assetsPath, path.resolve(distPath, assetsPath))
+    setState('Successfully built', 'success')
   } catch (e) {
-    throw e
+    setState(e.message, 'error')
   }
 }
 
